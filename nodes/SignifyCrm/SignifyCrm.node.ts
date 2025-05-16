619dd7d14a0af12ce286c06b61618945da9b1f65
serveradmin-git <server@signify.co.th>
2025-05-16 17:24:55 +0700

    Revert Changes because of incompability

--- nodes/SignifyCrm/SignifyCrm.node.ts
+++ nodes/SignifyCrm/SignifyCrm.node.ts
@@ -31,7 +31,7 @@ import type {
 	CamelCaseResource,
 } from './types';
 
-export default class SignifyCrm implements INodeType {
+export class SignifyCrm implements INodeType {
 	description: INodeTypeDescription = {
 		// Basic node details will go here
         displayName: 'SignifyCRM',
